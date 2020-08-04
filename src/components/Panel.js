43eecedcb6a2900bf76ca807d475d87ce2c0a6f8
Miguel <miguel.caravantes0@gmail.com>
2020-08-03 21:32:05 -0500

    add toolbar

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -3,12 +3,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Toolbar from '@material-ui/core/Toolbar';
+
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+
 import { Box } from '@material-ui/core';
 import SwipeableViews from 'react-swipeable-views';
 
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonPinIcon from '@material-ui/icons/PersonPin';
-import HomeIcon from '@material-ui/icons/Home';
+import HomeIcon from 'mdi-material-ui/Home';
+import MenuIcon from 'mdi-material-ui/Menu';
+import HeartIcon from 'mdi-material-ui/Heart';
 
 import Entities from './Entities';
 import Home from './home/Home';
@@ -22,6 +27,14 @@ const useStyles = makeStyles((theme) => ({
   },
   swipeableView: {
     flexGrow: 1,
+  },
+  menu: {
+    visibility: 'hidden',
+  },
+  '@media (max-width: 870px)': {
+    menu: {
+      visibility: 'visible',
+    },
   },
 }));
 
@@ -42,18 +55,38 @@ export default function Panel(props) {
     setValue(index);
   };
 
+  const handleMenuClick = () => {
+    window.parent.customPanel.parentNode.parentNode.offsetParent
+      .querySelector('home-assistant')
+      .shadowRoot.querySelector('home-assistant-main')
+      .dispatchEvent(new Event('hass-toggle-menu'));
+  };
+
   return (
     <div className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        centered>
-        <Tab icon={<HomeIcon />} aria-label="Home" />
-        <Tab icon={<FavoriteIcon />} aria-label="favorite" />
-        <Tab icon={<PersonPinIcon />} aria-label="person" />
-      </Tabs>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.menu}
+            onClick={handleMenuClick}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          centered>
+          <Tab icon={<HomeIcon />} aria-label="Home" />
+          <Tab icon={<HeartIcon />} aria-label="favorite" />
+          <Tab icon={<HeartIcon />} aria-label="person" />
+        </Tabs>
+      </AppBar>
       <SwipeableViews
         className={classes.swipeableView}
         index={value}
