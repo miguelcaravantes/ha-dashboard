e8d6e21ef95c879e160447e3cd5f1c7636cab68b
Miguel <miguel.caravantes0@gmail.com>
2020-08-04 19:37:01 -0500

    fix import

--- src/components/EntityPage.js
+++ src/components/EntityPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { List, Paper } from '@material-ui/core/List';
+import { List, Paper } from '@material-ui/core';
 
 import { useHass } from '../hooks/useHass';
 import EntityRow from './shared/EntityRow';
