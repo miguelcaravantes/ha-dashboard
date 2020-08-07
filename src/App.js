2e328d9d4c1c7909d05f320a83f78a712392527c
Miguel <miguel.caravantes0@gmail.com>
2020-08-07 12:56:14 -0500

    theme refactor, still only support for black theme

--- src/App.js
+++ src/App.js
@@ -9,19 +9,20 @@ import {
 } from '@material-ui/core/styles';
 import lightBlue from '@material-ui/core/colors/lightBlue';
 
+const prefersDarkMode = true; //useMediaQuery('(prefers-color-scheme: dark)');
 const theme = createMuiTheme({
   spacing: (factor) => `${factor * 8}px`,
   palette: {
-    type: 'dark',
+    type: prefersDarkMode ? 'dark' : 'light',
     primary: {
       main: lightBlue[300],
     },
-    background: {
-      default: '#000',
-      paper: 'rgba(11,11,11,.8)',
-    },
   },
 });
+if (prefersDarkMode) {
+  theme.palette.background.default = 'black';
+  theme.palette.background.paper = 'rgba(11,11,11,.8)';
+}
 
 export default function App(props) {
   return (
