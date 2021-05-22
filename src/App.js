56a528ec50e2de07c1e4d6604ab4a42152e8ae72
Miguel Caravantes <miguel.caravantes0@gmail.com>
2021-05-22 12:38:24 -0500

    update material ui

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { HassContext } from './common/HassContext';
 import { ThemeProvider } from '@emotion/react';
 import { Global, css } from '@emotion/react';
 import {
-  createMuiTheme,
+  createTheme,
   ThemeProvider as MuiThemeProvider,
-  StylesProvider,
 } from '@material-ui/core/styles';
+import { StylesProvider } from '@material-ui/styles';
 import lightBlue from '@material-ui/core/colors/lightBlue';
 
 const prefersDarkMode = true; //useMediaQuery('(prefers-color-scheme: dark)');
-const theme = createMuiTheme({
+const theme = createTheme({
   spacing: (factor) => `${factor * 8}px`,
   palette: {
     mode: prefersDarkMode ? 'dark' : 'light',
