d5f00f905d24ccfef3dcd5f9b4420ec549cb8418
Miguel Caravantes <miguelcaravantes@Miguels-MacBook-Pro.local>
2022-08-17 17:41:20 -0500

    fix font size

--- src/features/EntityCard/SensorDisplay.js
+++ src/features/EntityCard/SensorDisplay.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@mui/material';
 
 const SensorDisplay = ({ entity: { state, unitOfMeasurement }, className }) => (
-  <Box csx={{ fontSize: '1.5em', padding: '5px' }}>
+  <Box sx={{ fontSize: '1.5em', padding: '5px' }}>
     <span className={className}>
       {state}
       {unitOfMeasurement}
