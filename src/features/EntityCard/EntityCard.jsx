70987f92f50118ee74da219dea493ab2f89b5960
Miguel Caravantes <miguel@ski.com>
2025-01-11 15:04:22 -0600

    eslint

--- src/features/EntityCard/EntityCard.jsx
+++ src/features/EntityCard/EntityCard.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useCallback } from 'react';
+import { useMemo, useState, useCallback } from 'react';
 import { styled, keyframes, css as cssSystem } from '@mui/material/styles';
 import { Box } from '@mui/material';
 import ErrorBoundary from './ErrorBoundary';
