803a1deb2559b5ae6ea4813961467e0a06f7ff79
Miguel Caravantes <miguel@ski.com>
2026-01-28 00:21:11 -0600

    chore(03-03): remove migrated JSX files for EntityCard and Icons

--- src/features/EntityIcon.jsx
+++
@@ -1,7 +0,0 @@
-import Icon from './Icon';
-import useEntity from '../common/hooks/useEntity';
-
-export default function EntityIcon({ entityId }) {
-  const { icon } = useEntity(entityId);
-  return <Icon icon={icon} />;
-}
