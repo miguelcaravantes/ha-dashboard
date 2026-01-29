af63aa81fa5561abec1b089ea925188a3d3c5786
Miguel Caravantes <miguel@ski.com>
2026-01-28 23:26:36 -0600

    fix(05-02): resolve Stylis plugin type mismatch

--- src/common/MuiLayerProvider.tsx
+++ src/common/MuiLayerProvider.tsx
@@ -1,23 +1,29 @@
-import createCache from '@emotion/cache';
-import { CacheProvider } from '@emotion/react';
-import type { ReactNode } from 'react';
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
+import type { ReactNode } from "react";
 
-interface StylisElement {
+// Stylis plugin to wrap all rules in @layer mui
+// Based on https://github.com/emotion-js/emotion/issues/2422#issuecomment-884848529
+function stylisLayerPlugin(element: {
   type: string;
   value: string;
-  parent?: StylisElement;
+  parent: { type: string } | null;
+}) {
+  if (element.type === "rule" && element.parent?.type !== "@layer") {
+    element.value = `@layer mui { ${element.value} }`;
+  }
 }
 
 // Stylis plugin to wrap all rules in @layer mui
 // Based on https://github.com/emotion-js/emotion/issues/2422#issuecomment-884848529
 function stylisLayerPlugin(element: StylisElement) {
-  if (element.type === 'rule' && element.parent?.type !== '@layer') {
+  if (element.type === "rule" && element.parent?.type !== "@layer") {
     element.value = `@layer mui { ${element.value} }`;
   }
 }
 
 const cache = createCache({
-  key: 'mui-layer',
+  key: "mui-layer",
   stylisPlugins: [stylisLayerPlugin],
 });
 
